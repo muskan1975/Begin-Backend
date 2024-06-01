994c0b21d7e01421c950b582edc4748efd0781ad
muskan1975 <muskanambardekar76@gmail.com>
2024-06-01 21:05:58 +0530

    mm

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB                          
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/linkDuplicatorlink';
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://muskanambardekar76:muskansalim786@sms-soft.372qu1r.mongodb.net/?retryWrites=true&w=majority&appName=SMS-SOFT"
 mongoose.connect(MONGODB_URI, {})
   .then(() => {
     console.log('Connected to MongoDB');
